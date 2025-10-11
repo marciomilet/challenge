b8ae598e6463e38f7273f0b56a3792fb6ae2dd8e
marcio.milet <marcio.milet@sad.pe.gov.br>
2025-10-11 18:06:42 -0300

    feat file parsing endpoint

--- api/mysite/api/urls.py
+++ api/mysite/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserListCreate, CampaignListCreate, CampaignUserListCreate
+from .views import UserListCreate, CampaignListCreate, CampaignUserListCreate, upload
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='user-list-create'),
     path('campaigns/', CampaignListCreate.as_view(), name='campaign-list-create'),
     path('campaign-users/', CampaignUserListCreate.as_view(), name='campaignuser-list-create'),
+    path('upload/', upload, name='upload'),
 ]
--- api/mysite/api/views.py
+++ api/mysite/api/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
+from rest_framework.decorators import api_view,parser_classes
+from .services.csv_dict import csv_to_dict
 from .serializers import UserSerializer, CampaignSerializer, CampaignUserSerializer
 from .models import User, Campaign, CampaignUser
 
 class UserListCreate(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    
 
 class CampaignListCreate(generics.ListCreateAPIView):
     queryset = Campaign.objects.all()
@@ -16,3 +18,15 @@ class CampaignListCreate(generics.ListCreateAPIView):
 class CampaignUserListCreate(generics.ListCreateAPIView):    
     queryset = CampaignUser.objects.all()
     serializer_class = CampaignUserSerializer
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser])
+def upload(request):
+    users = csv_to_dict(request.data['users'])
+    print(users)
+    serializer = UserSerializer(data=users, many=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
