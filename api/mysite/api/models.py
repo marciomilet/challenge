1df55ee7cd4b864e769490344128b8b342531896
marcio.milet <marcio.milet@sad.pe.gov.br>
2025-10-12 21:57:17 -0300

    update Campaign model to allow null

--- api/mysite/api/models.py
+++ api/mysite/api/models.py
@@ -11,14 +11,16 @@ class User(models.Model):
 class Campaign(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    user_count = models.IntegerField(blank=True)
-    date = models.DateField(auto_now_add=True)
-    average_income = models.FloatField(blank=True)
+    user_count = models.IntegerField(null=True, blank=True)
+    date = models.DateField(auto_now_add=True, blank=True)
+    average_income = models.FloatField(null=True, blank=True)
 
 class CampaignUser(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    class Meta:
+        unique_together = ('user', 'campaign')
 
 def __str__(self):
     return self.id
